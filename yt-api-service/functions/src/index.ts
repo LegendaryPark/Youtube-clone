0ef00227076f61b918b3de957492737729caa374
legendaryPark <pbjkmn00@gmail.com>
2023-10-20 08:15:38 -0700

    Youtube api service with firebase

---
+++ yt-api-service/functions/src/index.ts
@@ -0,0 +1,49 @@
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
+initializeApp();
+const storage = new Storage();
+const rawVideoBucketName = "jp-yt-raw-videos";
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  const firestore = new Firestore();
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request)=>{
+  // Check if the user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated0"
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000,
+  });
+
+  return {url, fileName};
+});
